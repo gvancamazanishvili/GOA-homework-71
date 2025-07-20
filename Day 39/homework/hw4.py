9e9c3f1ac6dd55e2bec215930baf756d8f47757b
gvancamazanishvili <Mazanishviligvantsa@gmail.com>
2025-07-20 15:02:29 +0400

    commit

---
+++ Day 39/homework/hw4.py
@@ -0,0 +1,14 @@
+# 4) მომხმარებელს შემოატანინეთ სამი წინადადება და თითოეულზე გამოიყენეთ სხვადასხვა სტრინგის მეთოდი:
+# პირველ წინადადებაზე — .lower()
+# მეორე წინადადებაზე — .upper()
+# მესამე წინადადებაზე — .capitalize()
+
+
+sentence1 = input("Enter your sentence: ")
+sentence2 = input("Enter your sentence: ")
+sentence3 = input("Enter your sentence: ")
+
+
+print(sentence1.lower())
+print(sentence2.upper())
+print(sentence3.capitalize())
---
+++ Day 39/homework/hw5.py
@@ -0,0 +1,14 @@
+# 4) ცვლადში შეინახე შენი სახელი. მომხმარებელს შეეკითხე თავისი სახელი, იმ შემთხვევაში თუ თქვენი სახელები ემთხვევა დაბეჭდეთ "Our names are similar!", თუ არ ემთხვევა დაბეჭდეთ "We have different names". გაითვალისწინეთ, რომ მომხმარებელმა შეიძლება თქვენნაირი სახელი შემოიტანოს, თუმცა სახელის ასოები შესაძლოა იყოს სხვადასხვა შრიფტის ზომით, ამიტომ ამან თქვენ პროგრამაში შეფერხება არ უნდა გამოიწვიოს (გამოიყენეთ ნასწავლი სტრინგის მეთოდები)
+
+my_name = "Gvanca"
+
+name = input("Enter your name: ")
+
+if my_name.lower() == name.lower():
+    print("Our names are similar!")
+elif my_name.upper() == name.upper():
+    print("Our names are similar!")
+elif my_name.capitalize == name.capitalize:
+    print("Our names are similar!")
+else:
+    print("We have different names")
