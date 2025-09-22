46cb37470804b55b0357935940a26cc6abe4c9a8
gvancamazanishvili <Mazanishviligvantsa@gmail.com>
2025-09-22 15:19:56 +0400

    commit

---
+++ Day 65/classwork/cw1.py
@@ -0,0 +1,6 @@
+# სურვილისამებრ აარჩიეთ codewars-ები და შეასრულეთ
+# https://www.codewars.com/kata/search/?q=&r%5B%5D=-8&beta=false&order_by=sort_date%20desc\
+
+# ვერაფერი ვერ გავაკეთე
+
+# such a failure of me
---
+++ Day 65/homework/hw.py
@@ -0,0 +1,14 @@
+# შეასრულეთ ნებისმიერი 5 Codewars:
+# https://www.codewars.com/kata/search/?q=&r%5B%5D=-8&xids=completed&beta=false&order_by=sort_date%20desc
+
+# 1) 
+def remove_every_other(my_list):
+    return my_list[::2]
+
+# 2)
+def subtract_sum(number):
+    return "apple"
+
+# 3)
+def hex_to_dec(s):
+    return int(s,16)
